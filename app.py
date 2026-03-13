6943f018809f7091ebfe1e163a76d579e58b51be
Deepanshu <deepanshukanak@gmail.com>
2026-03-13 19:07:14 +0530

    fix: server_name for HF, gradio 4.44.1

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 from gradio_app import app
 
 if __name__ == "__main__":
-    app.launch()
+    app.launch(server_name="0.0.0.0", server_port=7860)
